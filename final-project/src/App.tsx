b58d996d0058d8ff1c789ee17aeaf2e3f208653f
Tonyf6 <149448421+Tonyf6@users.noreply.github.com>
2024-11-18 21:08:11 -0800

    BarberSelection Page

--- final-project/src/App.tsx
+++ final-project/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/login';
 import CreateAccount from './components/createaccount';
+import BarberSelection from './components/barberselection';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/createaccount" element={<CreateAccount />} />
+        <Route path="/barbers" element={<BarberSelection />} />
       </Routes>
     </Router>
   );
--- final-project/src/components/login.tsx
+++ final-project/src/components/login.tsx
@@ -1,47 +1,44 @@
-import logo from '../assets/Bad Guy Barbers Logo.png'
+import React from 'react';
+import logo from '../assets/Bad Guy Barbers Logo.png';
 
 const Login = () => {
   return (
     <div className="login-container">
       {/* Logo section */}
       <div className="logo-container">
-        <img src={logo} alt="Logo" className="logo" />
+        <img src={logo} alt="Bad Guy Barbers" className="logo" />
       </div>
 
       {/* Login box */}
       <div className="login-box">
         <h2 className="login-title">Login</h2>
-        <form className="flex flex-col gap-4">
-          {/* Username input */}
-          <label className="input-label" htmlFor="username">Username</label>
-          <input
-            type="text"
-            id="username"
-            className="input-field"
-            placeholder="Username"
-          />
-          {/* Password input */}
-          <label className="input-label" htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            className="input-field"
-            placeholder="Password"
-          />
-          {/* Login button */}
-          <button
-            type="submit"
-            className="button button-login"
-          >
-            Login
-          </button>
-          {/* Create Account button */}
-          <button
-            type="button"
-            className="button button-create-account"
-          >
-            Create Account
-          </button>
+        <form className="flex flex-col gap-6">
+          <div className="flex flex-col gap-2">
+            <label className="input-label">Username</label>
+            <input
+              type="text"
+              className="input-field"
+              placeholder="Enter username"
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label className="input-label">Password</label>
+            <input
+              type="password"
+              className="input-field"
+              placeholder="Enter password"
+            />
+          </div>
+
+          <div className="flex flex-col gap-3 mt-4">
+            <button type="submit" className="button button-login">
+              Login
+            </button>
+            <button type="button" className="button button-create-account">
+              Create Account
+            </button>
+          </div>
         </form>
       </div>
     </div>
